8831a38b3e089cc5aa16d2e5c80c7f4cd397b1fb
Halyna Koziak <galiakozyak@gmail.com>
2020-11-24 14:31:32 +0200

    Queue additions

--- src/test/java/ua/edu/ucu/collections/QueueTest.java
+++ src/test/java/ua/edu/ucu/collections/QueueTest.java
@@ -3,23 +3,20 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class QueueTest {
+    public Queue baseQueue = new Queue();
 
     @Test
     public void testQueuePeekAndEnQueue() {
-        Queue queue = new Queue();
-        queue.enQueue(13);
-
+        baseQueue.enQueue(13);
         int expected = 13;
-        int actual = (int) queue.peekTop();
+        int actual = (int) baseQueue.peekTop();
         assertEquals(expected, actual, 0.00001);
     }
 
     @Test
     public void testQueueDeQueue() {
-        Queue queue = new Queue();
-        queue.enQueue(13);
-
-        int actual = (int) queue.deQueue();
+        baseQueue.enQueue(13);
+        int actual = (int) baseQueue.deQueue();
         int expected = 13;
         assertEquals(actual, expected, 0.00001);
     }
