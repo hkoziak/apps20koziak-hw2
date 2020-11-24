28ace73776b3bcfebc036156835adaf1365bd75c
Halyna Koziak <galiakozyak@gmail.com>
2020-11-24 14:28:51 +0200

    Added Queue tests

--- src/test/java/ua/edu/ucu/collections/QueueTest.java
+++ src/test/java/ua/edu/ucu/collections/QueueTest.java
@@ -1,12 +1,27 @@
 package ua.edu.ucu.collections;
-
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class QueueTest {
-    
+
     @Test
-    public void testSomeMethod() {
+    public void testQueuePeekAndEnQueue() {
+        Queue queue = new Queue();
+        queue.enQueue(13);
+
+        int expected = 13;
+        int actual = (int) queue.peekTop();
+        assertEquals(expected, actual, 0.00001);
     }
-    
+
+    @Test
+    public void testQueueDeQueue() {
+        Queue queue = new Queue();
+        queue.enQueue(13);
+
+        int actual = (int) queue.deQueue();
+        int expected = 13;
+        assertEquals(actual, expected, 0.00001);
+    }
+
 }
