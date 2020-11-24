d9414501473ceaf9c04f2ff666e63feeb24122fe
Halyna Koziak <galiakozyak@gmail.com>
2020-11-24 14:35:11 +0200

    Added Stack tests

--- src/test/java/ua/edu/ucu/collections/StackTest.java
+++ src/test/java/ua/edu/ucu/collections/StackTest.java
@@ -1,12 +1,28 @@
 package ua.edu.ucu.collections;
-
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class StackTest {
-    
+    Stack baseStack = new Stack();
+
     @Test
-    public void testSomeMethod() {
+    public void testStackPush() {
+        baseStack.push(13);
+        int actual = (int) baseStack.peek();
+        int expected = 13;
+        assertEquals(expected, actual, 0.00001);
     }
-    
+
+    @Test
+    public void testStackPop() {
+        baseStack.push(13);
+        baseStack.push(12);
+        int actual1 = (int) baseStack.pop();
+        int actual2 = (int) baseStack.pop();
+        int expected1 = 12;
+        int expected2 = 13;
+        assertEquals(expected1, actual2, 0.00001);
+        assertEquals(expected1, actual2, 0.00001);
+
+    }
 }
